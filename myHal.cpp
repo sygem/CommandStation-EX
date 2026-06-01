a58adb20e8e2543952990e9eff84e9bf93889483
Jeff Vader <simon@sygem.com>
2026-06-01 09:50:19 +0100

    Updating vpins

--- myHal.cpp
+++ myHal.cpp
@@ -1,11 +1,11 @@
 #include "IODevice.h"
 
 void halSetup() {
-    PCA9685::create(100, 16, 0x40);
-    PCA9685::create(116, 16, 0x41, 1000);
-    PCA9685::create(132, 16, 0x42, 1000);
+    PCA9685::create(1000, 16, 0x40);
+    PCA9685::create(1016, 16, 0x41, 1000);
+    PCA9685::create(1032, 16, 0x42, 1000);
 
-    PCF8575::create(164, 16, 0x20);
-    PCF8575::create(180, 16, 0x21);
-    PCF8575::create(196, 16, 0x22);
+    PCF8575::create(1064, 16, 0x20);
+    PCF8575::create(1080, 16, 0x21);
+    PCF8575::create(1096, 16, 0x22);
 }
