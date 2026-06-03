bcd7d15e47716b49403f38676a9d301073e6e803
Jeff Vader <simon@sygem.com>
2026-06-03 13:47:28 +0100

    Add an additional PCF8575 for later use

--- myHal.cpp
+++ myHal.cpp
@@ -1,11 +1,16 @@
 #include "IODevice.h"
+#include "IO_PCF8575.h"
 
 void halSetup() {
     PCA9685::create(1000, 16, 0x40);
     PCA9685::create(1016, 16, 0x41, 1000);
     PCA9685::create(1032, 16, 0x42, 1000);
 
+    // 3 PCF8575s for the control panel buttons and LEDs.  Allocates 48 pins 1064-1111.
     PCF8575::create(1064, 16, 0x20);
     PCF8575::create(1080, 16, 0x21);
     PCF8575::create(1096, 16, 0x22);
+
+    PCF8575::create(1112, 16, 0x23); // spare expander for future use.  Allocates pins 1112-1127.
+
 }
